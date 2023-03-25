75fa8018edfca7a6ea97d03f00c3e33d860b4fa3
Clément Hue <clementhue@hotmail.fr>
2023-03-25 14:41:19 +0100

    mapping sdl_scancode done

--- Ugine/src/utils/keyboard_mapping.h
+++ Ugine/src/utils/keyboard_mapping.h
@@ -6,6 +6,6 @@
 
 namespace ugine::utils {
     ImGuiKey keycode_to_imguikey(ugine::utils::keycode key);
-    ugine::utils::keycode sdl_keycode_to_keycode(SDL_Keycode key);
+    ugine::utils::keycode sdl_scancode_to_keycode(SDL_Keycode key);
     SDL_Scancode keycode_to_sdl_scancode(ugine::utils::keycode key);
 }
